806388e8bdd10eb0e67798c6ada76f07e2726d32
noahvarghese <varghese.noah@gmail.com>
2019-11-25 22:44:38 -0500

    switched words around in about page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -8,7 +8,7 @@ class About extends Component {
             <div className="aboutStyle">
               <h1>About</h1>
               <hr />
-              <p class="about">Hello, my name is Noah Varghese.<span>I am a full stack developer based in Oakville Ontario. I'm extremely passionate about web development and design, as well as helping businesses build and improve their online presence. I enjoy programming, playing guitar, exploring and pushing the limits.</span></p>
+              <p class="about">Hello, my name is Noah Varghese.<span>I am a full stack developer based in Oakville Ontario. I'm extremely passionate about web design and development, as well as helping businesses build and improve their online presence. I enjoy programming, playing guitar, exploring and pushing the limits.</span></p>
             </div>
         </React.Fragment>
     );
