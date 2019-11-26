1b220293b4aaf3021a2ed3142f7daffe420e0b67
noahvarghese <varghese.noah@gmail.com>
2019-11-25 22:40:17 -0500

    changed about page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -8,10 +8,7 @@ class About extends Component {
             <div className="aboutStyle">
               <h1>About</h1>
               <hr />
-              <br />
-              <p>Noah found his passion for computers after high school. It wasn’t until he was studying business at McMaster that he got his first computer. As soon as he saw his first terminal and entered his first commands using the Bash shell, he was hooked.</p>
-              <br />
-              <p>Not long after, he fell down the rabbit hole that was GNU/Linux, compiled his first kernel, and discovered he wanted to pursue software development and computer science as a career.</p>
+              <p>My name is Noah Varghese. <span>I am a full stack developer with a passion for web design and development. I'm looking to help businesses build and improve their online presence. My passions are programming, playing guitar, exploring and pushing the limits.</span></p>
             </div>
         </React.Fragment>
     );
