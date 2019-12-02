e06a24d504b35054d1ef4d5968b1dcd0489df653
noahvarghese <varghese.noah@gmail.com>
2019-12-01 21:38:03 -0500

    https to http on project links

--- src/components/pages/Experience.js
+++ src/components/pages/Experience.js
@@ -12,15 +12,15 @@ class Experience extends Component {
               <div className="projectStyle">
                 <h2>Projects</h2>
                 <br />
-                  <h4><a className="linkStyle" href="https://www.noahvarghese.me/projects/todo/">React Todo App</a></h4>
+                  <h4><a className="linkStyle" href="http://www.noahvarghese.me/projects/todo/">React Todo App</a></h4>
                   <br />
                   <p>A simple todo list written using React. Was used to get familiar with React and state management.</p>  
                   <br />
-                  <h4><a className="linkStyle" href="https://github.com/noahvarghese/userApi">Authentication Api</a></h4>
+                  <h4><a className="linkStyle" href="http://github.com/noahvarghese/userApi">Authentication Api</a></h4>
                   <br />
                   <p>A restful api that handles user authentication using json web tokens. Written in PHP and uses a mySQL database. Demonstrates secure user authentication techniques, as well as database security and scripting.</p>
                   <br />
-                  <h4><a className="linkStyle" href="https://www.noahvarghese.me/projects/restaurant">Restaurant Generator</a></h4>
+                  <h4><a className="linkStyle" href="http://www.noahvarghese.me/projects/restaurant">Restaurant Generator</a></h4>
                   <br />
                   <p>A single page web app written in JavaScript and using Bootstrap for styling. Was used to get familiar with bootstrap and responsive design. Utilizes the geolocation api, as well as Yelp's api. Demonstrates familiarity with ajax and json.</p>  
               </div>
