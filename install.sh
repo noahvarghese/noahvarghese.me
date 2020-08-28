3994713370c8be20363169cc1e66afe0d25f96d0
noahvarghese <varghese.noah@gmail.com>
2020-08-28 13:35:58 -0400

    corgot top part of config

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ mkdir ~/.aws
 touch ~/.aws/{credentials,config}
 sudo echo "aws_access_key_id = $AWS_KEY" > ~/.aws/credentials
 sudo echo "aws_secret_access_key = $AWS_SECRET" >> ~/.aws/credentials
-
-sudo echo "region = ca-central-1" > ~/.aws/config
+sudo echo "[default]" > ~/.aws/config
+sudo echo "region = ca-central-1" >> ~/.aws/config
 sudo echo "output = json" >> ~/.aws/config
 npm install && npm audit fix
