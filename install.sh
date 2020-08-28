18e14aa5b252e703508b073a101973cc95809f56
noahvarghese <varghese.noah@gmail.com>
2020-08-28 13:23:41 -0400

    sudo erything

--- install.sh
+++ install.sh
@@ -3,10 +3,10 @@
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install
-touch /home/travis/.aws/{credentials,config}
-echo "aws_access_key_id = $AWS_KEY" > /home/travis/.aws/credentials
-echo "aws_secret_access_key = $AWS_SECRET" >> /home/travis/.aws/credentials
+touch /root/.aws/{credentials,config}
+sudo echo "aws_access_key_id = $AWS_KEY" > /root/.aws/credentials
+sudo echo "aws_secret_access_key = $AWS_SECRET" >> /home/travis/.aws/credentials
 
-echo "region = ca-central-1" > /home/travis/.aws/config
-echo "output = json" >> /home/travis/.aws/config
+sudo echo "region = ca-central-1" > /root/.aws/config
+sudo echo "output = json" >> /home/travis/.aws/config
 npm install && npm audit fix
