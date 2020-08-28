2395ee6c6c21bc5c8a511cb1ece0f380fbeeca54
noahvarghese <varghese.noah@gmail.com>
2020-08-28 13:30:37 -0400

    tilde

--- install.sh
+++ install.sh
@@ -3,10 +3,10 @@
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install
-touch /root/.aws/{credentials,config}
-sudo echo "aws_access_key_id = $AWS_KEY" > /root/.aws/credentials
-sudo echo "aws_secret_access_key = $AWS_SECRET" >> /home/travis/.aws/credentials
+touch ~/.aws/{credentials,config}
+sudo echo "aws_access_key_id = $AWS_KEY" > ~/.aws/credentials
+sudo echo "aws_secret_access_key = $AWS_SECRET" >> ~/.aws/credentials
 
-sudo echo "region = ca-central-1" > /root/.aws/config
-sudo echo "output = json" >> /home/travis/.aws/config
+sudo echo "region = ca-central-1" > ~/.aws/config
+sudo echo "output = json" >> ~/.aws/config
 npm install && npm audit fix
