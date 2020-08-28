99ba5e792c35fde9bd248a7973c26db65da9fd9e
noahvarghese <varghese.noah@gmail.com>
2020-08-28 13:21:05 -0400

    touch both files before editig\ng

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install
+touch /home/travis/.aws/{credentials,config}
 echo "aws_access_key_id = $AWS_KEY" > /home/travis/.aws/credentials
 echo "aws_secret_access_key = $AWS_SECRET" >> /home/travis/.aws/credentials
 
