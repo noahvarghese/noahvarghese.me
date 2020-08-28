fe6b5480cfdf9c48e8cdd43723c837b948de5594
noahvarghese <varghese.noah@gmail.com>
2020-08-28 13:32:33 -0400

    mkdir

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install
+mkdir ~/.aws
 touch ~/.aws/{credentials,config}
 sudo echo "aws_access_key_id = $AWS_KEY" > ~/.aws/credentials
 sudo echo "aws_secret_access_key = $AWS_SECRET" >> ~/.aws/credentials
