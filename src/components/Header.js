7918f36684ec78e57252c3b9c1134852d81d8c4d
noahvarghese <varghese.noah@gmail.com>
2020-08-28 22:18:25 -0400

    specify ico

--- src/components/Header.js
+++ src/components/Header.js
@@ -134,6 +134,7 @@ const Header = ({data}) => {
             <Helmet>
                 <title>{ title }</title>
                 <meta name="description" content={description}/>
+                <link rel="icon" type="image/ico" href="favicon.ico"/>
             </Helmet>
             { 
                 isMobile && 
