b588cb1428d99981ab658c8d8a195eb096e87ed0
Noah Varghese <varghese.noah@gmail.com>
2021-03-11 03:41:15 -0500

    added email valiation and commented out signup

--- server/src/lib/routes/api/auth/index.ts
+++ server/src/lib/routes/api/auth/index.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from "express";
 import passport from "passport";
 import bcrypt from "bcrypt";
 import User from "../../../models/users";
+import validator from "validator";
 
 const router = express.Router();
 
@@ -10,22 +11,28 @@ router.post("/login", passport.authenticate("local", { failureRedirect: "/login"
 });
 
 // we dont want signup as it is only i that will use the backend
-router.post("/signup", async (req: Request, res: Response) => {
-    User.findOne(req.body, async (err: any, user: any) => {
-        if (err) {
-            res.sendStatus(400);
-        }
-        if (user) {
-            res.status(200);
-            res.send({ message: "Email already registered." });
-        }
-        else {
-            const hash = bcrypt.hashSync(req.body.password, 10);
-            const user = await User.create({ ...req.body, hash });
-            await user.save();
-            res.sendStatus(200);
-        }
-    });
-});
+// router.post("/signup", async (req: Request, res: Response) => {
+//     if (validator.isEmail(req.body.email)) {
+//         User.findOne(req.body, async (err: any, user: any) => {
+//             if (err) {
+//                 res.sendStatus(400);
+//             }
+//             if (user) {
+//                 res.status(200);
+//                 res.send({ message: "Email already registered." });
+//             }
+//             else {
+//                 const hash = bcrypt.hashSync(req.body.password, 10);
+//                 const user = await User.create({ ...req.body, hash });
+//                 await user.save();
+//                 res.sendStatus(200);
+//             }
+//         });
+//     }
+//     else {
+//         res.status(400);
+//         res.send({ message: "Invalid email." });
+//     }
+// });
 
 export default router;
