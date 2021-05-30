362020a203e69f3be0c3b95fbdb9928339a1904f
noahvarghese <varghese.noah@gmail.com>
2021-05-30 00:07:09 +0000

    fixed error

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,16 +1,24 @@
-import * as React from "React";
+import * as React from "react";
 import { SkillLevel, technical_skills } from "../data/technical_skills";
 import { interpersonal_skills } from "../data/interpersonal_skills";
 import "./home.scss";
 
 export const HomeComponent = () => {
-    const conversant = technical_skills.filter((skill) => skill.level === SkillLevel.CONVERSANT);
-    const proficient = technical_skills.filter(skill => skill.level === SkillLevel.PROFICIENT);
+    const conversant = technical_skills.filter(
+        (skill) => skill.level === SkillLevel.CONVERSANT
+    );
+    const proficient = technical_skills.filter(
+        (skill) => skill.level === SkillLevel.PROFICIENT
+    );
 
     return (
         <>
             <div id="message">
-                <p>Hi! Welcome to my website, whether you're looking for someone to jumpstart your next venture, find out more about me, or looking to hire, feel free to look around!</p>
+                <p>
+                    Hi! Welcome to my website, whether you're looking for
+                    someone to jumpstart your next venture, find out more about
+                    me, or looking to hire, feel free to look around!
+                </p>
             </div>
             <div id="about">
                 <div id="stack">
@@ -26,7 +34,11 @@ export const HomeComponent = () => {
                     </div>
                     <div id="interpersonal">
                         <h3>Interpersonal Skills</h3>
-                        <ul>{interpersonal_skills.map((skill, index) => <li key={index}>{skill}</li>)}</ul>
+                        <ul>
+                            {interpersonal_skills.map((skill, index) => (
+                                <li key={index}>{skill}</li>
+                            ))}
+                        </ul>
                     </div>
                 </div>
                 <div id="technical">
@@ -34,12 +46,18 @@ export const HomeComponent = () => {
                     <ul>
                         <li>
                             <p>Proficient In:</p>
-                            <ul>{proficient.map((skill, index) => <li key={index}>{skill.name}</li>)}</ul>    
+                            <ul>
+                                {proficient.map((skill, index) => (
+                                    <li key={index}>{skill.name}</li>
+                                ))}
+                            </ul>
                         </li>
                         <li>
                             <p>Conversant In:</p>
                             <ul>
-                                {conversant.map((skill, index) => <li key={index}>{skill.name}</li>)}
+                                {conversant.map((skill, index) => (
+                                    <li key={index}>{skill.name}</li>
+                                ))}
                             </ul>
                         </li>
                     </ul>
@@ -47,4 +65,4 @@ export const HomeComponent = () => {
             </div>
         </>
     );
-}
+};
