fc8a5ec4ca4fe56afb4eff85c55bab58c382aa0d
noahvarghese <varghese.noah@gmail.com>
2022-03-03 16:07:06 +0000

    fixed alt tag

--- src/components/project.jsx
+++ src/components/project.jsx
@@ -2,16 +2,18 @@ import * as React from "react";
 import "./project.scss";
 import WWW from "../images/ic_public_24px.png";
 
-export const Project = ({project}) => {
+export const Project = ({ project }) => {
     return (
         <div className="project">
             <div className="proj-header">
                 <h4 className="proj-name">{project.name}</h4>
                 <div className="proj-url-container">
                     <div className="proj-url-img-container">
-                        <img alt="globe image symbolizing world wide web" src={WWW} />
+                        <img alt="globe symbolizing world wide web" src={WWW} />
                     </div>
-                    <h4 className="proj-url"><a href={`https://${project.url}`}>{project.url}</a></h4>
+                    <h4 className="proj-url">
+                        <a href={`https://${project.url}`}>{project.url}</a>
+                    </h4>
                 </div>
                 <h4 className="tags">{project.tags.join(" | ")}</h4>
             </div>
