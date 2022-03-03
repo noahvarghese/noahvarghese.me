f1d35fcaa829561ccbafdf5962b2e2303af69e89
noahvarghese <varghese.noah@gmail.com>
2022-03-03 16:00:02 +0000

    code formatting, image alt

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -7,40 +7,57 @@ import Email from "../images/ic_email_24px.png";
 import "./header.scss";
 
 export const Header = () => {
-    return ( 
+    return (
         <div id="header">
             <div id="info">
                 <ul>
                     <li>
                         <div className="infoImgContainer">
-                            <img alt="My hometown" src={Home}/>
+                            <img alt="My hometown" src={Home} />
                         </div>
                         <p>Oakville ON, Canada</p>
                     </li>
                     <li>
                         <div className="infoImgContainer">
-                            <img alt="My GitHub" src={Github}/>
+                            <img alt="My GitHub" src={Github} />
                         </div>
-                        <a href="https://github.com/noahvarghese" target="_blank" rel="noreferrer">GitHub.com/noahvarghese</a>
+                        <a
+                            href="https://github.com/noahvarghese"
+                            target="_blank"
+                            rel="noreferrer"
+                        >
+                            GitHub.com/noahvarghese
+                        </a>
                     </li>
                     <li>
                         <div className="infoImgContainer">
-                            <img alt="My LinkedIn" src={Linkedin}/>
+                            <img alt="My LinkedIn" src={Linkedin} />
                         </div>
-                        <a href="https://linkedin.com/in/varghese-noah" target="_blank" rel="noreferrer">LinkedIn.com/in/varghese-noah</a>
+                        <a
+                            href="https://linkedin.com/in/varghese-noah"
+                            target="_blank"
+                            rel="noreferrer"
+                        >
+                            LinkedIn.com/in/varghese-noah
+                        </a>
                     </li>
                     <li>
                         <div className="infoImgContainer">
-                            <img alt="My email" src={Email}/>
+                            <img alt="My email" src={Email} />
                         </div>
-                        <a href="mailto:varghese.noah@gmail.com" target="_blank" rel="noreferrer">varghese.noah@gmail.com</a>
+                        <a
+                            href="mailto:varghese.noah@gmail.com"
+                            target="_blank"
+                            rel="noreferrer"
+                        >
+                            varghese.noah@gmail.com
+                        </a>
                     </li>
                 </ul>
             </div>
             <div id="me">
-                <img src={Me} alt="a picture of me" />
+                <img src={Me} alt="Noah Varghese" />
             </div>
         </div>
-
     );
-}
+};
