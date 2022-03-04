d60ee2a22ce24d9013e9785ed954ef4ce6a7fd56
noahvarghese <varghese.noah@gmail.com>
2022-03-04 16:15:42 +0000

    adjusted 404 page

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,55 +1,48 @@
 import * as React from "react";
-import { Link } from "gatsby";
+import { Helmet } from "react-helmet";
+import Footer from "../components/footer";
+import { Nav } from "../components/nav";
 
-// styles
-const pageStyles = {
-    color: "#232129",
-    padding: "96px",
-    fontFamily: "-apple-system, Roboto, sans-serif, serif",
-};
-const headingStyles = {
-    marginTop: 0,
-    marginBottom: 64,
-    maxWidth: 320,
-};
-
-const paragraphStyles = {
-    marginBottom: 48,
-};
-const codeStyles = {
-    color: "#8A6534",
-    padding: 4,
-    backgroundColor: "#FFF4DB",
-    fontSize: "1.25rem",
-    borderRadius: 4,
-};
-
-// markup
 const NotFoundPage = () => {
     return (
-        <main style={pageStyles}>
-            HIIIII
-            <title>Not found</title>
-            <h1 style={headingStyles}>Page not found</h1>
-            <p style={paragraphStyles}>
-                Sorry{" "}
-                <span role="img" aria-label="Pensive emoji">
-                    😔
-                </span>{" "}
-                we couldn’t find what you were looking for.
-                <br />
-                {process.env.NODE_ENV === "development" ? (
-                    <>
-                        <br />
-                        Try creating a page in{" "}
-                        <code style={codeStyles}>src/pages/</code>.
-                        <br />
-                    </>
-                ) : null}
-                <br />
-                <Link to="/">Go home</Link>.
-            </p>
-        </main>
+        <>
+            <header>
+                <Helmet>
+                    <meta charset="utf-8" />
+                    <title>Noah Varghese &middot; 404</title>
+                    <meta
+                        name="viewport"
+                        content="width=device-width, initial-scale=1"
+                    ></meta>
+                </Helmet>
+                <Nav />
+            </header>
+            <main
+                style={{
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                }}
+            >
+                <div
+                    style={{
+                        display: "flex",
+                        flexDirection: "column",
+                        gap: "2rem",
+                    }}
+                >
+                    <h1>Page not found</h1>
+                    <p>
+                        Sorry{" "}
+                        <span role="img" aria-label="Pensive emoji">
+                            😔
+                        </span>{" "}
+                        we couldn’t find what you were looking for.
+                    </p>
+                </div>
+            </main>
+            <Footer />
+        </>
     );
 };
 
